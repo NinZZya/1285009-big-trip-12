3c65325deae99bbd20c0c1eee1a306e598ee6aa2
NinZZya <s.a.azarov@gmail.com>
2020-07-29 01:27:58 +0300

    Add Trip Filters

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import {createTemplateTripInfo} from './components/trip-info';
 import {createTemplateTripTabs} from './components/trip-tabs';
+import {createTemplateTripFilters} from './components/trip-filters';
 import {
   RenderPosition,
   render,
@@ -7,12 +8,12 @@ import {
 } from './utils/utils';
 
 const tripMain = document.querySelector(`.trip-main`);
-const tripControls = tripMain.querySelector(`.trip-controls`);
-
 const tripInfo = createElement(createTemplateTripInfo());
+render(tripMain, tripInfo, RenderPosition.AFTERBEGIN);
+
+const tripControls = tripMain.querySelector(`.trip-controls`);
 const tripTabs = createElement(createTemplateTripTabs());
 const tripTabsPlace = tripMain.querySelectorAll(`h2`)[1];
-
-render(tripMain, tripInfo, RenderPosition.AFTERBEGIN);
 render(tripControls, tripTabs, tripTabsPlace);
-
+const tripFilters = createElement(createTemplateTripFilters());
+render(tripControls, tripFilters, RenderPosition.BEFOREEND);
