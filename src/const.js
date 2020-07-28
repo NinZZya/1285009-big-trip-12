1edeb8b16c1886b15aa88c232e627caa2edfd36e
NinZZya <s.a.azarov@gmail.com>
2020-07-29 02:41:49 +0300

    Add tripFilters and tripSorts

---
+++ src/const.js
@@ -0,0 +1,18 @@
+const tripFilters = new Map([
+  [`everything`, `Everything`],
+  [`future`, `Future`],
+  [`past`, `Past`],
+]);
+
+const tripSorts = new Map([
+  [`day`, `Day`],
+  [`event`, `Event`],
+  [`time`, `Time`],
+  [`price`, `Price`],
+  [`offers`, `Offers`],
+]);
+
+export {
+  tripFilters,
+  tripSorts,
+};
