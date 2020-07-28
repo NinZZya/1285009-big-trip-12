ba795fe9cbb4076c6f7f9d2d988295c9dd0f0768
NinZZya <s.a.azarov@gmail.com>
2020-07-29 01:22:41 +0300

    Add Trip Tabs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+import {createTemplateTripInfo} from './components/trip-info';
+import {createTemplateTripTabs} from './components/trip-tabs';
 import {
   RenderPosition,
   render,
   createElement,
 } from './utils/utils';
 
-import {createTemplateTripInfo} from './components/trip-info';
-
 const tripMain = document.querySelector(`.trip-main`);
+const tripControls = tripMain.querySelector(`.trip-controls`);
+
 const tripInfo = createElement(createTemplateTripInfo());
+const tripTabs = createElement(createTemplateTripTabs());
+const tripTabsPlace = tripMain.querySelectorAll(`h2`)[1];
 
 render(tripMain, tripInfo, RenderPosition.AFTERBEGIN);
+render(tripControls, tripTabs, tripTabsPlace);
+
