05a55bd90d88dafeeba022ee8e7d1e50fc4d1b25
NinZZya <s.a.azarov@gmail.com>
2020-08-03 18:53:34 +0300

    Add controls directory. Rename TripControls to Controls. Code review

---
+++ src/components/controls/controls.js
@@ -0,0 +1,10 @@
+const createControlsTemplate = () => {
+  return (
+    `<div class="trip-main__trip-controls  trip-controls">
+      <h2 id="trip-header-swich-view" class="visually-hidden">Switch trip view</h2>
+      <h2 id="trip-filter-events" class="visually-hidden">Filter events</h2>
+    </div>`
+  );
+};
+
+export {createControlsTemplate};
