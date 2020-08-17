86bedde9d7e4186f35f8ea2dab75a880a402d392
NinZZya <s.a.azarov@gmail.com>
2020-08-17 14:47:34 +0300

    Review utils

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,12 +1,25 @@
 
-const RenderPosition = {
+import AbstractView from '../view/abstract/abstract';
+
+export const RenderPosition = {
   BEFORE_BEGIN: `BEFORE_BEGIN`,
   AFTER_BEGIN: `AFTER_BEGIN`,
   BEFORE_END: `BEFORE_END`,
   AFTER_END: `AFTER_END`,
 };
 
-const render = (element1, element2, place = RenderPosition.BEFORE_END) => {
+export const render = (element1, element2, place = RenderPosition.BEFORE_END) => {
+  const getElement = (element) => {
+    if (element instanceof AbstractView) {
+      return element.getElement();
+    }
+
+    return element;
+  };
+
+  element1 = getElement(element1);
+  element2 = getElement(element2);
+
   switch (place) {
     case RenderPosition.BEFORE_BEGIN:
       element1.before(element2);
@@ -25,15 +38,9 @@ const render = (element1, element2, place = RenderPosition.BEFORE_END) => {
   }
 };
 
-const createElement = (template) => {
+export const createElement = (template) => {
   const element = document.createElement(`div`);
   element.innerHTML = template;
 
   return element.firstChild;
-};
-
-export {
-  RenderPosition,
-  render,
-  createElement,
 };
