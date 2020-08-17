9c482d1f6965261977d0344787458afb03440c1d
NinZZya <s.a.azarov@gmail.com>
2020-08-17 21:47:18 +0300

    WIP. Add listemers for point editor

--- src/view/point-editor/point-editor.js
+++ src/view/point-editor/point-editor.js
@@ -19,10 +19,43 @@ class PointEditorView extends AbstractView {
     super();
     this._point = point;
     this._destinations = destinations;
+    this._formSubmitHandler = this._formSubmitHandler.bind(this);
+    this._formResetHandler = this._formResetHandler.bind(this);
+    this._rollupButtonClickHandler = this._rollupButtonClickHandler.bind(this);
   }
 
   getTemplate() {
     return createPointEditorTemplate(this._point, this._destinations);
+  }
+
+  _formSubmitHandler(evt) {
+    evt.preventDefault();
+    this._callback.formSubmit();
+  }
+
+  _formResetHandler(evt) {
+    evt.preventDefault();
+    this._callback.formReset();
+  }
+
+  _rollupButtonClickHandler(evt) {
+    evt.preventDefault();
+    this._callback._rollupButtonClick();
+  }
+
+  setFormSubmitHandler(callback) {
+    this._callback.formSubmit = callback;
+    this.getElement().addEventListener(`submit`, this._formSubmitHandler);
+  }
+
+  setFormResetHandler(callback) {
+    this._callback.formReset = callback;
+    this.getElement().addEventListener(`reset`, this._formResetHandler);
+  }
+
+  setRollupButtonClickHandler(callback) {
+    this._callback._rollupButtonClick = callback;
+    this.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, this._rollupButtonClickHandler);
   }
 }
 
