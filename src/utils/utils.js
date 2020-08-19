46e7d08f1027c998f80b2c2f8a9a3144a5166f6b
NinZZya <s.a.azarov@gmail.com>
2020-08-19 13:33:15 +0300

    Review utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,8 @@
-const ESK_KEYCODE = 27;
+const Key = {
+  ESCAPE: `Escape`,
+  ESC: `Esc`,
+};
 
 export const toFirstUpperCase = (word) => word[0].toUpperCase() + word.slice(1);
 export const extend = (a, ...b) => Object.assign({}, a, ...b);
-export const isEscPressed = (evt) => evt.keyCode === ESK_KEYCODE;
+export const isEscPressed = (evt) => evt.key === Key.ESCAPE || evt.key === Key.ESC;
