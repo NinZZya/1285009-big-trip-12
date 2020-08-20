dd1161504f1a32c967d92c7b33f0523a6a8454a2
NinZZya <s.a.azarov@gmail.com>
2020-08-20 20:34:25 +0300

    Review sort utils

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -1,3 +1,2 @@
 export const sortPointDurationDown = (pointA, pointB) => pointB.duration - pointA.duration;
 export const sortPointPriceDown = (pointA, pointB) => pointB.price - pointA.price;
-export const sortPointStartUp = (pointA, pointB) => pointB.start - pointA.start;
