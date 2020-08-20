af2f109ff2e8267e08aeeabd2726cf227caa0dce
NinZZya <s.a.azarov@gmail.com>
2020-08-20 19:27:31 +0300

    Add sort utils

---
+++ src/utils/sort.js
@@ -0,0 +1,3 @@
+export const sortPointByDuration = (pointA, pointB) => pointB.duration - pointA.duration;
+export const sortPointByPrice = (pointA, pointB) => pointB.price - pointA.price;
+export const sortPointByStart = (pointA, pointB) => pointB.start - pointA.start;
