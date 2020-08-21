4d722a81b36191d29e374f71fc28fa5d379fef9c
NinZZya <s.a.azarov@gmail.com>
2020-08-21 16:34:35 +0300

    Add id in mock data

--- src/mock/points.js
+++ src/mock/points.js
@@ -1,3 +1,4 @@
+import {nanoid} from 'nanoid';
 import {
   MoveDate,
   getRandomBool,
@@ -90,6 +91,7 @@ const generatePoint = () => {
   )));
 
   return {
+    id: nanoid(),
     type: getRandomArrayValue(POINT_TYPES),
     destination: getRandomArrayValue(DESTINATIONS),
     start: dateStart,
