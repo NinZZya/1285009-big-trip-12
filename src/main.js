018e78a34e55e72c9fa8f3c4d60fa8dc2a0e735c
NinZZya <s.a.azarov@gmail.com>
2020-08-25 22:00:00 +0300

    WIP. Add trip model to main

--- src/main.js
+++ src/main.js
@@ -8,22 +8,11 @@ import {
   StatisticsView,
 } from './view/';
 
-import {
-  TripPresenter
-} from './presenter';
-
-import {
-  RenderPosition,
-  render,
-} from './utils/dom';
-
-import {
-  generatePoints,
-} from './mock/points';
-
-import {
-  DESTINATIONS,
-} from './mock/points';
+import {TripPresenter} from './presenter';
+import {TripModel} from './model';
+import {RenderPosition, render} from './utils/dom';
+import {generatePoints} from './mock/points';
+import {DESTINATIONS} from './mock/points';
 
 const {
   BEFORE_BEGIN,
@@ -33,6 +22,10 @@ const {
 
 const POINTS_COUNT = 20;
 const points = generatePoints(POINTS_COUNT);
+
+const tripModel = new TripModel();
+tripModel.setDestinations(DESTINATIONS);
+tripModel.setPoints(points);
 
 const tripMainElement = document.querySelector(`.trip-main`);
 const infoView = new InfoView();
@@ -51,5 +44,5 @@ render(tripMainElement, newPointButtonView, BEFORE_END);
 
 const tripEventsElement = document.querySelector(`.trip-events`);
 
-const tripPresenter = new TripPresenter(tripEventsElement);
+const tripPresenter = new TripPresenter(tripEventsElement, tripModel);
 tripPresenter.init(points, DESTINATIONS);
