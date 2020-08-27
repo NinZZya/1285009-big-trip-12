bb2d8e871668d68ae58a66481956318697deca1f
NinZZya <s.a.azarov@gmail.com>
2020-08-28 00:57:22 +0300

    Review utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
+import {convertNumberOfDate, convertMsToDHM} from './date';
+
 const Key = {
   ESCAPE: `Escape`,
   ESC: `Esc`,
@@ -6,3 +8,13 @@ const Key = {
 export const toFirstUpperCase = (word) => word[0].toUpperCase() + word.slice(1);
 export const extend = (a, ...b) => Object.assign({}, a, ...b);
 export const isEscPressed = (evt) => evt.key === Key.ESCAPE || evt.key === Key.ESC;
+
+export const convertDurationValue = (duration) => {
+  duration = convertMsToDHM(duration);
+  const {days, hours, minutes} = duration;
+  if (days > 0) {
+    return `${convertNumberOfDate(days)}D ${convertNumberOfDate(hours)}H ${convertNumberOfDate(minutes)}M`;
+  }
+
+  return `${hours > 0 ? `${convertNumberOfDate(hours)}H` : ``} ${convertNumberOfDate(minutes)}M`;
+};
