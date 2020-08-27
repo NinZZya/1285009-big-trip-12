833efccd1a84e6caff0ece9f349a4f6131cade29
NinZZya <s.a.azarov@gmail.com>
2020-08-28 00:59:38 +0300

    WIP. Review TabsView

--- src/view/tabs/tabs.js
+++ src/view/tabs/tabs.js
@@ -2,15 +2,17 @@ import AbstractView from '../abstract';
 import {TabItem} from '../../const';
 
 const TABS = Object.values(TabItem);
+const ACTIVE_TAB_CLASS = `trip-tabs__btn--active`;
 
-const createTabsTemplate = (ativeTab) => {
+const createTabsTemplate = () => {
   return (
     `<nav class="trip-controls__trip-tabs  trip-tabs">
       ${TABS
         .map((tab) => (
           `<a
-            class="trip-tabs__btn ${tab === ativeTab ? `trip-tabs__btn--active` : ``}"
+            class="trip-tabs__btn ${tab === TabItem.TABLE ? ACTIVE_TAB_CLASS : ``}"
             href="#"
+            data-tab="${tab}"
           >
             ${tab}
           </a>`
@@ -21,12 +23,31 @@ const createTabsTemplate = (ativeTab) => {
 };
 
 export default class Tabs extends AbstractView {
-  constructor(ativeTab) {
+  constructor() {
     super();
-    this._ativeTab = ativeTab;
+    this._tabsClickHandler = this._tabsClickHandler.bind(this);
   }
 
   getTemplate() {
-    return createTabsTemplate(this._ativeTab);
+    return createTabsTemplate();
+  }
+
+  _tabsClickHandler(evt) {
+    evt.preventDefault();
+    const prevActiveTabElement = this.getElement().querySelector(`.${ACTIVE_TAB_CLASS}`);
+    const prevActiveTab = prevActiveTabElement.dataset.tab;
+    prevActiveTabElement.classList.remove(ACTIVE_TAB_CLASS);
+
+    evt.target.classList.add(ACTIVE_TAB_CLASS);
+    const activeTab = evt.target.dataset.tab;
+
+    if (activeTab !== prevActiveTab) {
+      this._callback.tabsClick(activeTab);
+    }
+  }
+
+  setTabsClickHandler(callback) {
+    this._callback.tabsClick = callback;
+    this.getElement().addEventListener(`click`, this._tabsClickHandler);
   }
 }
