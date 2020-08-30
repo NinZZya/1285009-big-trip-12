d85ceeebe22c8747e788253dca07c4f0c42e5a42
NinZZya <s.a.azarov@gmail.com>
2020-08-30 19:45:36 +0300

    WIP. Add disable method and set handler

--- src/view/new-point-button/new-point-button.js
+++ src/view/new-point-button/new-point-button.js
@@ -12,7 +12,25 @@ const createNewPointButtonTemplate = () => {
 };
 
 export default class NewPointButton extends AbstractView {
+  constructor() {
+    super();
+    this._newPointButtonClickHandler = this._newPointButtonClickHandler.bind(this);
+  }
   getTemplate() {
     return createNewPointButtonTemplate();
+  }
+
+  disabled(isDisabled) {
+    this.getElement().disabled = isDisabled;
+  }
+
+  _newPointButtonClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.newPointButtonClick();
+  }
+
+  setNewPointButtonClickHandler(callback) {
+    this._callback.newPointButtonClick = callback;
+    this.getElement().addEventListener(`click`, this._newPointButtonClickHandler);
   }
 }
